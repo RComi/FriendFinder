4010f9bcafdd933a28ab64f5d29199ddb079d18c
RC <recomi40@gmail.com>
2018-04-19 20:42:34 -0400

    commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,5 @@
+// import { ok } from 'assert';
+
 var path = require('path');
 
 var friends = require('../data/friends.js');
@@ -9,6 +11,34 @@ module.exports = function (app) {
     });
 
     app.post('/api/friends', function (req, res) {
-       friends.push(req.body);    
+        console.log ("REQ " + req);
+        var friendName = "";
+        var friendPhoto = "";
+        var userAboutInfo = req.body;
+        var userResponse = userAboutInfo.ratings;
+        var totalDiff = 40;
+
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0;
+
+            for (var j = 0; j < userResponse.length; j++) {
+                diff += Math.abs(friends[i].ratings[j] - userResponse[j]);
+            }
+
+            if (diff < totalDiff) {
+                totalDiff = diff;
+                friendName = friends[i].name;
+                friendPhoto = friends[i].photo;
+            }
+        }
+
+        friends.push(req.body);
+
+        res.json({
+            name: friendName,
+            photo: friendPhoto
+        });
+
+        console.log("RES" + res);
     });
 };
